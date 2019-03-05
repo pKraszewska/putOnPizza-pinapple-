10016e131a86d9ab64fd49bcc1a180c794084403
pKraszewska <paula.kraszewska@gmail.com>
2019-03-05 15:27:20 +0100

    created equpment class

--- src/java/com.codecool.equipment/View/EquipmentView.java
+++ src/java/com.codecool.equipment/View/EquipmentView.java
@@ -1,4 +1,25 @@
 package com.codecool.equipment.View;
 
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
 public class EquipmentView {
+
+
+
+    private VBox view = new VBox();
+
+    public EquipmentView() {
+        view.setPadding(new Insets(15));
+        view.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
+    }
+
+
+    public VBox getView() {
+        return view;
+    }
 }
