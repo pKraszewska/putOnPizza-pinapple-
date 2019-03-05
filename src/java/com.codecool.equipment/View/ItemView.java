fbdbebf1719b5dbf7983db9907ff20fc36d453ff
Szymon Nowak <sznowak13@gmail.com>
2019-03-05 14:58:49 +0100

    Basic ItemView impl

--- src/java/com.codecool.equipment/View/ItemView.java
+++ src/java/com.codecool.equipment/View/ItemView.java
@@ -2,9 +2,13 @@ package com.codecool.equipment.View;
 
 import com.codecool.equipment.Model.Item;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class ItemView extends HBox {
 
     public ItemView(Item item) {
+        Text name = new Text(item.getName());
+        Text weight = new Text(String.valueOf(item.getWeight()) + " kg");
+        getChildren().addAll(name, weight);
     }
 }
