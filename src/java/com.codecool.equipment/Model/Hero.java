e3ef5743c3c8e26503dbb7cdf58af1e089236c24
bdziedzic <bartosz.dziedzic94@gmail.com>
2019-03-05 14:21:36 +0100

    create hero and item class

--- src/java/com.codecool.equipment/Model/Hero.java
+++ src/java/com.codecool.equipment/Model/Hero.java
@@ -1,4 +1,24 @@
 package com.codecool.equipment.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private final int CAPACITY = 200;
+    private List<Item> itemList = new ArrayList<>();
+
+
+    private void addItem(Item item){
+
+    }
+
+    private void removeItem(Item item){
+
+    }
+
+
+
+
+    }
+
 }
--- src/java/com.codecool.equipment/Model/Item.java
+++ src/java/com.codecool.equipment/Model/Item.java
@@ -1,4 +1,19 @@
 package com.codecool.equipment.Model;
 
 public class Item {
+    private int weight;
+    private String name;
+    private int id;
+    private static int itemCounter = 0;
+
+
+
+
+
+    public Item(int weight, String name, int id) {
+        this.weight = weight;
+        this.name = name;
+        this.id = itemCounter++;
+
+    }
 }
