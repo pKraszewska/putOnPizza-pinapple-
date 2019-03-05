2cab91aeebee4aa0078cc1ac78a99a1cc5ebc8ea
pKraszewska <paula.kraszewska@gmail.com>
2019-03-05 14:55:30 +0100

    heroImage, maxCapacity, currentCapacity added to HeroView

--- src/java/com.codecool.equipment/View/MainView.java
+++ src/java/com.codecool.equipment/View/MainView.java
@@ -1,8 +1,15 @@
 package com.codecool.equipment.View;
 
 
-import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
 public class MainView extends BorderPane {
+
+    private HeroView heroView;
+
+    public MainView(HeroView heroView) {
+        this.setCenter(heroView.getView());
+    }
+
+
 }
