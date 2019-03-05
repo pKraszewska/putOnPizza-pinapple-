afcc4bda78a9aac107ac3a8215887fdcabfd685a
Szymon Nowak <sznowak13@gmail.com>
2019-03-05 14:15:24 +0100

    Added ItemListView containing available items

---
+++ src/java/com.codecool.equipment/View/ItemListView.java
@@ -0,0 +1,26 @@
+package com.codecool.equipment.View;
+
+import com.codecool.equipment.Model.Item;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class ItemListView extends VBox {
+
+    ItemListView() {
+        super(5);
+        setPadding(new Insets(15));
+        setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
+    }
+
+    public void updateView(List<Item> items) {
+        for (Item item : items) {
+            getChildren().add(new ItemView(item));
+        }
+    }
+
+}
