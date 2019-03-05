9ff951f9749a816ec7617f86074027c9f599bf5b
Szymon Nowak <sznowak13@gmail.com>
2019-03-05 16:07:19 +0100

    Added scaling of the window

--- src/java/com.codecool.equipment/Config.java
+++ src/java/com.codecool.equipment/Config.java
@@ -1,8 +1,12 @@
 package com.codecool.equipment;
 
+import java.awt.*;
+
 public class Config {
 
-    public static final int WINDOW_WIDTH = 1400;
-    public static final int WINDOW_HEIGHT = 900;
+    public static final double SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    public static final double SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    public static final double WINDOW_WIDTH = SCREEN_WIDTH * 0.75;
+    public static final double WINDOW_HEIGHT = SCREEN_HEIGHT * 0.75;
 
 }
