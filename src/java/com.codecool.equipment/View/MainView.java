35b5653c3da3700feb49b41bc74030c7d2727e0f
pKraszewska <paula.kraszewska@gmail.com>
2019-03-05 15:26:43 +0100

    added equipmentview to mainview constructor

--- src/java/com.codecool.equipment/View/MainView.java
+++ src/java/com.codecool.equipment/View/MainView.java
@@ -6,9 +6,11 @@ import javafx.scene.layout.BorderPane;
 public class MainView extends BorderPane {
 
     private HeroView heroView;
+    private EquipmentView equipmentView;
 
-    public MainView(HeroView heroView) {
+    public MainView(HeroView heroView, EquipmentView equipmentView) {
         this.setCenter(heroView.getView());
+        this.setLeft(equipmentView.getView());
     }
 
 
