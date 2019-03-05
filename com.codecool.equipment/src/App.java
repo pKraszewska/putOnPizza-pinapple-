bcbf36c86f8aa445f39a1b8da328378b03f7b2cf
bdziedzic <bartosz.dziedzic94@gmail.com>
2019-03-05 10:56:58 +0100

    modules

---
+++ com.codecool.equipment/src/App.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Scene creationScene = new Scene(form, ViewConfig.WIDTH, ViewConfig.HEIGHT);
+    }
+}
