be0ab554556ece0db322432c7bf92cb4f3d43d37
Szymon Nowak <sznowak13@gmail.com>
2019-03-06 09:36:12 +0100

    Added updateView method in ItemListView
    it converts item List to fit into GridPane

--- src/java/com.codecool.equipment/View/ItemListView.java
+++ src/java/com.codecool.equipment/View/ItemListView.java
@@ -1,11 +1,11 @@
 package com.codecool.equipment.View;
 
+import com.codecool.equipment.Config;
 import com.codecool.equipment.Model.Item;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 import java.util.List;
@@ -17,9 +17,26 @@ public class ItemListView extends GridPane {
         setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
     }
 
+    /**
+     * Updates the list of available items in the view.
+     * Because the list is a GridPane (a 2d matrix with rows and columns) and the data comes as
+     * simple 1d list we need to fill up the correct row and column with every item from the list.
+     * To get correct row index we divide the item original index by fixed number of columns in the grid.
+     * To get the correct column index we take modulo of the fixed column number.
+     * (e.g. i = 7, fixed num = 4
+     * 7 / 4 = 1(.75)
+     * 7 % 4 = 3
+     * so the list[7] becomes matrix[1][3].
+     *
+     * @see Config#ITEM_GRID_COLUMN_NUMBER
+     *
+     * @param items
+     */
     public void updateView(List<Item> items) {
-        for (Item item : items) {
-            
+        for (int i = 0; i < items.size(); i++) {
+            int row = i / Config.ITEM_GRID_COLUMN_NUMBER;
+            int col = i % Config.ITEM_GRID_COLUMN_NUMBER;
+            add(new UnequippedItemView(items.get(i)), col ,row);
         }
     }
 
