b9c045445d8637f85d97ec39f8e94ddad89701e2
pKraszewska <paula.kraszewska@gmail.com>
2019-03-07 11:34:50 +0100

    implemented getter for heroView and EquipmentListView

--- src/main/java/com.codecool.equipment/View/MainView.java
+++ src/main/java/com.codecool.equipment/View/MainView.java
@@ -5,18 +5,31 @@ import com.codecool.equipment.DisplayConfig;
 import javafx.geometry.Insets;
 import javafx.scene.layout.BorderPane;
 
+import java.util.Observer;
+
 public class MainView extends BorderPane {
 
     private HeroView heroView;
     private ItemListView itemListView;
     private EquipmentListView equipmentListView;
 
-    public MainView(HeroView heroView, ItemListView itemListView, EquipmentListView equipmentListView) {
+    public MainView(HeroView heroView, ItemListView itemListView, EquipmentListView equipmentListView, Observer mainObserver) {
         setPadding(new Insets(DisplayConfig.PADDING));
+        this.heroView = heroView;
+        this.itemListView = itemListView;
+        this.equipmentListView = equipmentListView;
+        itemListView.addObserver(mainObserver);
         this.setCenter(heroView.getView());
-        this.setRight(itemListView);
+        this.setRight(itemListView.getGridPane());
         this.setLeft(equipmentListView.getView());
     }
 
 
+    public HeroView getHeroView() {
+        return heroView;
+    }
+
+    public EquipmentListView getEquipmentView() {
+        return equipmentListView;
+    }
 }
