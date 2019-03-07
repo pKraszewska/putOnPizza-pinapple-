93f1f0186a01496e4c9fe41c7adb4e146f25b475
Szymon Nowak <sznowak13@gmail.com>
2019-03-07 21:18:11 +0100

    Added handlers in EquipmentView
    When clicking on item that is container
    it sends request to controller to change the view based on that item id
    When browsing container the back button request going back to the
    previous container

--- src/main/java/com.codecool.equipment/View/EquipmentListView.java
+++ src/main/java/com.codecool.equipment/View/EquipmentListView.java
@@ -1,33 +1,59 @@
 package com.codecool.equipment.View;
 
 import com.codecool.equipment.DisplayConfig;
+import com.codecool.equipment.Model.Container;
+import com.codecool.equipment.Model.Hero;
 import com.codecool.equipment.Model.Item;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.util.List;
+import java.util.Observable;
 
-public class EquipmentListView {
+public class EquipmentListView extends Observable {
 
     private VBox view = new VBox();
+    private VBox equipmentView = new VBox();
+
+    private Text equipmentLabel = new Text();
+    private Button backButton = new Button("Back");
 
     public EquipmentListView() {
         view.setPrefSize(DisplayConfig.SIDE_PANE_SIZE_WIDTH, DisplayConfig.SIDE_PANE_SIZE_HEIGHT);
         view.setPadding(new Insets(DisplayConfig.PADDING));
         view.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
+        backButton.setOnAction(e -> sendGoBackRequest());
+        HBox equipmentHeader = new HBox();
+        equipmentHeader.getChildren().addAll(equipmentLabel, backButton);
+        view.getChildren().addAll(equipmentHeader, equipmentView);
     }
 
-    public void updateView(List<Item> items) {
-        view.getChildren().clear();
-        for (Item item : items) {
-            view.getChildren().add(new EquippedItemView(item));
+    private void sendGoBackRequest() {
+        setChanged();
+        notifyObservers("BACK");
+    }
+
+    public void updateView(Container container) {
+        equipmentView.getChildren().clear();
+        equipmentLabel.setText(container.getName() + " items: \n");
+        backButton.setVisible(!(container instanceof Hero));
+        for (Item item : container.getContainedItems()) {
+            equipmentView.getChildren().add(new EquippedItemView(item, this));
         }
     }
 
     public VBox getView() {
         return view;
+    }
+
+    public void sendBrowsingRequest(int containerId) {
+        setChanged();
+        notifyObservers(containerId);
     }
 }
--- src/main/java/com.codecool.equipment/View/EquippedItemView.java
+++ src/main/java/com.codecool.equipment/View/EquippedItemView.java
@@ -1,14 +1,18 @@
 package com.codecool.equipment.View;
 
+import com.codecool.equipment.Model.Container;
 import com.codecool.equipment.Model.Item;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 public class EquippedItemView extends HBox {
 
-    public EquippedItemView(Item item) {
+    public EquippedItemView(Item item, EquipmentListView root) {
         Text t1 = new Text(item.getName());
         Text t2 = new Text(String.valueOf(item.getWeight()));
         getChildren().addAll(t1, t2);
+        if (item instanceof Container) {
+            setOnMouseClicked(e -> root.sendBrowsingRequest(item.getId()));
+        }
     }
 }
