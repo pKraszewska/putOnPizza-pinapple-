3a79903559e8305c57ca04a995cb27d52d11a104
pKraszewska <paula.kraszewska@gmail.com>
2019-03-07 11:37:35 +0100

    initialize MainController

--- src/main/java/com.codecool.equipment/App.java
+++ src/main/java/com.codecool.equipment/App.java
@@ -1,4 +1,7 @@
 package com.codecool.equipment;
+import com.codecool.equipment.Controller.AvailableItemsController;
+import com.codecool.equipment.Controller.HeroController;
+import com.codecool.equipment.Controller.MainController;
 import com.codecool.equipment.View.EquipmentListView;
 import com.codecool.equipment.View.HeroView;
 import com.codecool.equipment.View.ItemListView;
@@ -13,8 +16,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        MainView mainView = new MainView(new HeroView(), new ItemListView(), new EquipmentListView());
-        Scene mainScene = new Scene(mainView, DisplayConfig.WINDOW_WIDTH, DisplayConfig.WINDOW_HEIGHT, new Color(0.48,0.60,0.65,1));
+        MainController mainController = new MainController();
+        Scene mainScene = new Scene(mainController.getMainView(), DisplayConfig.WINDOW_WIDTH, DisplayConfig.WINDOW_HEIGHT, new Color(0.48,0.60,0.65,1));
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
