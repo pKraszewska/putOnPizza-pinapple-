1aed4723ab4da4d8218066667c3a1cdb954b5f05
pKraszewska <paula.kraszewska@gmail.com>
2019-03-07 11:33:14 +0100

    IteamListView extends obserable, getter created, sendIdToDelete method implemented

--- src/main/java/com.codecool.equipment/View/ItemListView.java
+++ src/main/java/com.codecool.equipment/View/ItemListView.java
@@ -6,16 +6,21 @@ import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 import java.util.List;
+import java.util.Observable;
 
-public class ItemListView extends GridPane {
+public class ItemListView extends Observable {
+
+    GridPane gridPane = new GridPane();
 
     public ItemListView() {
-        setPadding(new Insets(15));
-        setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
-        setPrefSize(DisplayConfig.SIDE_PANE_SIZE_WIDTH, DisplayConfig.SIDE_PANE_SIZE_HEIGHT);
+        gridPane.setPadding(new Insets(15));
+        gridPane.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
+        gridPane.setPrefSize(DisplayConfig.SIDE_PANE_SIZE_WIDTH, DisplayConfig.SIDE_PANE_SIZE_HEIGHT);
+        gridPane.add(new UnequippedItemView(new Item(45, "Test"), this), 0, 0);
     }
 
     /**
@@ -37,8 +42,16 @@ public class ItemListView extends GridPane {
         for (int i = 0; i < items.size(); i++) {
             int row = i / DisplayConfig.ITEM_GRID_COLUMN_NUMBER;
             int col = i % DisplayConfig.ITEM_GRID_COLUMN_NUMBER;
-            add(new UnequippedItemView(items.get(i)), col ,row);
+            getGridPane().add(new UnequippedItemView(items.get(i), this), col ,row);
         }
     }
 
+    public GridPane getGridPane() {
+        return gridPane;
+    }
+
+    public void sendIdtoDelete(int itemId) {
+        setChanged();
+        notifyObservers(itemId);
+    }
 }
