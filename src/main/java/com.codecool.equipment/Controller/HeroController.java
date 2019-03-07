a009343994a9826cf56afebcf0a0fd3208547611
Szymon Nowak <sznowak13@gmail.com>
2019-03-07 21:20:59 +0100

    Added handling for equipmentView events

--- src/main/java/com.codecool.equipment/Controller/HeroController.java
+++ src/main/java/com.codecool.equipment/Controller/HeroController.java
@@ -10,21 +10,37 @@ public class HeroController {
 
     private Hero heroModel;
     private HeroView heroView;
+    private Container browsedContainer;
     private EquipmentListView equipmentListView;
 
     public HeroController(Hero heroModel, HeroView heroView, EquipmentListView equipmentListView) {
         this.heroModel = heroModel;
         this.heroView = heroView;
         this.equipmentListView = equipmentListView;
+        this.browsedContainer = heroModel;
+        updateDisplay();
     }
 
     public void updateDisplay() {
         heroView.updateDisplay(heroModel);
-        equipmentListView.updateView(heroModel.getItemList());
+        equipmentListView.updateView(browsedContainer);
     }
 
     public void equipItem(Item item) {
         heroModel.equip(item);
+        updateDisplay();
+    }
+
+    public void changeEquipmentView(int containerId) {
+        Container container = heroModel.getInnerContainer(containerId);
+        if (container != null) {
+            browsedContainer = container;
+            updateDisplay();
+        }
+    }
+
+    public void browseBack() {
+        browsedContainer = browsedContainer.getOwner();
         updateDisplay();
     }
 }
