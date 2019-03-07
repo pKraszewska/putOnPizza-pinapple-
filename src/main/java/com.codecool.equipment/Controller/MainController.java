a7c381029cde1610f3a7401c037f334062e3232e
pKraszewska <paula.kraszewska@gmail.com>
2019-03-07 11:29:40 +0100

    implemented observer to MainController with update method

--- src/main/java/com.codecool.equipment/Controller/MainController.java
+++ src/main/java/com.codecool.equipment/Controller/MainController.java
@@ -1,20 +1,41 @@
 package com.codecool.equipment.Controller;
 
 import com.codecool.equipment.Model.Hero;
+import com.codecool.equipment.Model.Item;
+import com.codecool.equipment.View.EquipmentListView;
 import com.codecool.equipment.View.HeroView;
+import com.codecool.equipment.View.ItemListView;
+import com.codecool.equipment.View.MainView;
+import javafx.scene.Parent;
 
-public class MainController {
+import java.util.Observable;
+import java.util.Observer;
 
-    private Hero heroModel;
-    private HeroView heroView;
+public class MainController implements Observer {
 
-    public MainController(Hero heroModel, HeroView heroView) {
-        this.heroModel = heroModel;
-        this.heroView = heroView;
+    private HeroController heroController;
+    private MainView mainView;
+    private AvailableItemsController availableItemsController;
+
+    public MainController() {
+        this.mainView = new MainView(new HeroView(), new ItemListView(), new EquipmentListView(), this);
+        this.heroController = new HeroController(new Hero(), mainView.getHeroView(), mainView.getEquipmentView());
+        availableItemsController = new AvailableItemsController();
+
     }
 
-    public void updateDisplay() {
-        heroView.updateDisplay(heroModel);
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof ItemListView) {
+            System.out.println("Kliknięty został");
+            int itemId = (Integer) arg;
+            Item item = availableItemsController.takeItem(itemId);
+            heroController.equipItem(item);
+        }
+
     }
 
+    public MainView getMainView() {
+        return mainView;
+    }
 }
