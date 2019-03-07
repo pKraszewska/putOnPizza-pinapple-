4495ae7554df5189bec7f96f528b74c064035935
pKraszewska <paula.kraszewska@gmail.com>
2019-03-07 09:53:35 +0100

    sample css added to equipmnent list view

--- src/main/java/com.codecool.equipment/View/EquipmentListView.java
+++ src/main/java/com.codecool.equipment/View/EquipmentListView.java
@@ -8,16 +8,18 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+import java.net.URL;
 import java.util.List;
 
 public class EquipmentListView {
 
     private VBox view = new VBox();
 
+
     public EquipmentListView() {
         view.setPrefSize(DisplayConfig.SIDE_PANE_SIZE_WIDTH, DisplayConfig.SIDE_PANE_SIZE_HEIGHT);
         view.setPadding(new Insets(DisplayConfig.PADDING));
-        view.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
+        view.setStyle(cssLayout);
     }
 
     public void updateView(List<Item> items) {
@@ -29,4 +31,11 @@ public class EquipmentListView {
     public VBox getView() {
         return view;
     }
+
+    String cssLayout = "-fx-border-color: red;\n" +
+            "-fx-border-insets: 1;\n" +
+            "-fx-border-width: 3;\n" +
+            "-fx-border-style: dashed;\n"+
+            "-fx-background-color: lightgray;\n";
+
 }
