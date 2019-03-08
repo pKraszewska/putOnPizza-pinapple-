f15042c4601483969dd24994f8b91b7845c3727c
pKraszewska <paula.kraszewska@gmail.com>
2019-03-08 08:54:44 +0100

    createResourcesFileNAme and checkIfResExists moved to Util class

--- src/main/java/com.codecool.equipment/View/AvailableItemView.java
+++ src/main/java/com.codecool.equipment/View/AvailableItemView.java
@@ -20,7 +20,7 @@ public class AvailableItemView extends VBox {
       setPadding(new Insets(DisplayConfig.SIDE_PANE_PADDING));
       Text name = new Text(item.getName());
       Text weight = new Text(String.valueOf(item.getWeight()));
-      this.itemImage = new ImageView((new Image(createResourcesFileName(item))));
+      this.itemImage = new ImageView((new Image(Util.createResourcesFileName(item))));
       itemImage.setFitHeight(DisplayConfig.ITEM_IMAGE_SIZE);
       itemImage.setFitWidth(DisplayConfig.ITEM_IMAGE_SIZE);
       itemId = item.getId();
@@ -29,21 +29,5 @@ public class AvailableItemView extends VBox {
           if (e.getClickCount() == 2) root.sendIdtoDelete(itemId);
       });
   }
-
-    private String createResourcesFileName(Item item) {
-        List<String> extensions = Arrays.asList(".png", ".jpg", ".gif");
-        String searchedFileName = "bomb.jpg";
-        for (String extension : extensions) {
-            searchedFileName = item.getName() + extension;
-            if (checkIfResourceExists(searchedFileName)) {
-                break;
-            }
-        }
-        return searchedFileName;
-    }
-
-    private boolean checkIfResourceExists(String fileName) {
-        return getClass().getResource("/" + fileName) != null;
-    }
 
 }
