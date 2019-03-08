66111b4e6d2c0ca8efbe9611f78c79728da40bf2
pKraszewska <paula.kraszewska@gmail.com>
2019-03-08 08:57:57 +0100

    cssLayout moved to util class with new name listCssLayout

--- src/main/java/com.codecool.equipment/View/EquipmentListView.java
+++ src/main/java/com.codecool.equipment/View/EquipmentListView.java
@@ -19,7 +19,7 @@ public class EquipmentListView {
         view.setPrefSize(DisplayConfig.SIDE_PANE_SIZE_WIDTH, DisplayConfig.SIDE_PANE_SIZE_HEIGHT);
         view.setPadding(new Insets(DisplayConfig.PADDING));
         view.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200 ,200), null, null)));
-        view.setStyle(cssLayout);
+        view.setStyle(Util.listCssLayout);
     }
 
     public void updateView(List<Item> items) {
@@ -35,11 +35,4 @@ public class EquipmentListView {
         return view;
     }
 
-    String cssLayout = "-fx-border-color: darkblue;\n" +
-            "-fx-border-insets: 0.5;\n" +
-            "-fx-border-width: 3;\n" +
-            "-fx-border-style: dashed;\n"+
-            "-fx-border-radius: 20;\n"+
-            "-fx-background-color: lightgray;\n"+
-            "-fx-background-radius: 20;\n";
 }
